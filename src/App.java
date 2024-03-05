b0bf07efdeb9da7bb0a1d726fc1fb749f9933ff0
Yagmur Bozdag <yagmurbozdag@Muhammed-MacBook-Air-2.local>
2024-03-05 11:59:03 +0300

    first commit

---
+++ src/App.java
@@ -0,0 +1,11 @@
+public class App {
+
+
+    public static int compute(Integer[] arr, Integer[] arr2, int var1, int var2){
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(compute(new Integer[0], new Integer[0], 1, 2));
+    }
+}
