2be97b3fe1ae754e72971a0dd3f1ab7067ba1d4a
zyamur <zeynepbzd04@gmail.com>
2024-03-06 14:27:34 +0300

    Dev:added command to method compute

--- src/App.java
+++ src/App.java
@@ -1,11 +1,15 @@
 public class App {
 
 
-    public static int compute(Integer[] arr, Integer[] arr2, int var1, int var2){
-        return -1;
-    }
+    //Our method going to get first strings i'th indexes which are represented in the first array
+    //and same for the second one than concat the selected strings.
+    public static int compute(Integer[] arr, Integer[] arr2, String str1, String str2){
 
+
+        return 0;
+
+    }
     public static void main(String[] args) {
-        System.out.println(compute(new Integer[0], new Integer[0], 1, 2));
+        System.out.println(compute(new Integer[0], new Integer[0], "1", "2"));
     }
 }
