f096006cded8c4a6ee5d1ceb25b01ed44496fa00
zyamur <zeynepbzd04@gmail.com>
2024-03-08 23:38:53 +0300

    Dev:added methods to apptest class(added junit library using maven) and made some changes in app class compute method

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,16 +8,20 @@ public class App {
     public static String compute(Integer[] arr1, Integer[] arr2, String str1, String str2){
         StringBuilder result = new StringBuilder();
 
+        if (arr1 == null || arr2 == null || str1 == null || str2 == null) {
+            return "";
+        }
+
         //Gets the wanted string parts from arr1
         for (int index : arr1) {
-            if (index >= 0 && index < str1.length()) {
+            if (index >= 0 && index < str1.length() ) {
                 result.append(str1.charAt(index));
             }
         }
 
         //Gets the wanted string parts from arr2
         for (int index : arr2) {
-            if (index >= 0 && index < str2.length()) {
+            if (index >= 0 && index < str2.length() ) {
                 result.append(str2.charAt(index));
             }
         }
--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -1,4 +1,78 @@
 package test.java.com.mycompany.app;
 
+import main.java.com.mycompany.app.App;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class AppTest {
+
+    @Test
+    void testComputeValidIndexes() {
+        Integer[] arr1 = {1, 3};
+        Integer[] arr2 = {2};
+        String str1 = "Alone";
+        String str2 = "Warrior";
+        String expected = "lnr";
+        assertEquals(expected, App.compute(arr1, arr2, str1, str2));
+    }
+
+    @Test
+    void testComputeEmptyArrays() {
+        Integer[] arr1 = {};
+        Integer[] arr2 = {};
+        String str1 = "Notso";
+        String str2 = "much";
+        String expected = "";
+        assertEquals(expected, App.compute(arr1, arr2, str1, str2));
+    }
+
+    @Test
+    void testComputeOutOfRange() {
+        Integer[] arr1 = {6};
+        Integer[] arr2 = {1, 3};
+        String str1 = "Rick";
+        String str2 = "Morty";
+        String expected = "ot";
+        assertEquals(expected, App.compute(arr1, arr2, str1, str2));
+    }
+
+    @Test
+    void testComputeEmptyStrings() {
+        Integer[] arr1 = {1, 2};
+        Integer[] arr2 = {0};
+        String str1 = "";
+        String str2 = "";
+        String expected = "";
+        assertEquals(expected, App.compute(arr1, arr2, str1, str2));
+    }
+
+    @Test
+    void testComputeNullStrings() {
+        Integer[] arr1 = {1, 2};
+        Integer[] arr2 = {0};
+        String str1 = null;
+        String str2 = null;
+        String expected = "";
+        assertEquals(expected, App.compute(arr1, arr2, str1, str2));
+    }
+
+    @Test
+    void testComputeNullArrays() {
+        Integer[] arr1 = null;
+        Integer[] arr2 = null;
+        String str1 = "Nobody";
+        String str2 = "Knows";
+        String expected = "";
+        assertEquals(expected, App.compute(arr1, arr2, str1, str2));
+    }
+
+    @Test
+    void testComputeWithMixedValidAndInvalidIndices() {
+        Integer[] arr1 = {1, 6};
+        Integer[] arr2 = {0, 3};
+        String str1 = "Me";
+        String str2 = "LikeYou";
+        String expected = "eLe";
+        assertEquals(expected, App.compute(arr1, arr2, str1, str2));
+    }
 }
